77f865a29185339806259feed308b62eb9fa8376
dE5pEradojr <funky@iMac-funky.local>
2014-10-09 15:57:30 +0300

    MU
    Fibo

---
+++ section-1.swift
@@ -0,0 +1,19 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+func getFibonacchi(mArg: Int) -> (mArg: Int, mResultNum: Int){
+    var mResultNum = 0;
+    var mFirstNum = 0;
+    var mSecondNum = 1;
+  
+    for i in 2..<mArg { //если по новому, то 2...mArg
+        mResultNum = mFirstNum + mSecondNum
+        mFirstNum = mSecondNum
+        mSecondNum = mResultNum
+    }
+   
+return (mArg, mResultNum)
+}
+
+println(getFibonacchi(10))
